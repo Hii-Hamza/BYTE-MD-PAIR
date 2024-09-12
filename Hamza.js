849ba09c788ea3c3bd7061e789c1aaf04e8349d9
Hamza <124891005+Hii-Hamza@users.noreply.github.com>
2024-09-12 20:02:48 +0500

    Update Hamza.js

--- Hamza.js
+++ Hamza.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
+__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
+
 let server = require('./qr'),
     code = require('./pair');
+
 require('events').EventEmitter.defaultMaxListeners = 500;
+
 app.use('/qr', server);
 app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+app.use('/pair', async (req, res, next) => {
+    res.sendFile(__path + '/pair.html');
+});
+app.use('/', async (req, res, next) => {
+    res.sendFile(__path + '/main.html');
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
+
+// Listen on 0.0.0.0 to make it accessible externally
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`
 Powered by Hamza
 
- Server running on http://localhost:` + PORT)
-})
+Server running on port: ${PORT}
+`);
+});
 
-module.exports = app
+module.exports = app;
